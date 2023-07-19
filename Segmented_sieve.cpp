a29245a761fffa968bc66dbccad61fd8c77f041c
Goelankita15 <117450446+Goelankita15@users.noreply.github.com>
2023-07-19 14:09:59 +0530

    Create Segmented_sieve.cpp

---
+++ Segmented_sieve.cpp
@@ -0,0 +1,10 @@
+L R -> difference sholud be  10^6
+
+generate primes till sqrt(R)
+create dummy array of R,- L + 1 size
+mark all multiples of prime till sqrt(R) not prime
+
+
+Factorisation using sieve
+set all numbers as primes
+for each prime check if -1 is present then place a[j] = i;
